07e78bb71c13f88686f25168af900d3f10680baa
Raphael Marcelino <110142433+raphaelfmarcelino@users.noreply.github.com>
2022-09-21 11:29:36 -0300

    arrays e loops

---
+++ javascript-para-iniciantes/arrays-e-loops/script.js
@@ -0,0 +1,15 @@
+var videoGames = ['Switch', 'PS4', 'Xbox'];
+
+var ultimoItem = videoGames.pop();
+
+videoGames.push('3DS');
+
+// for (var numero = 0; numero <= 10; numero++) {
+//     console.log(numero);
+// }
+
+var i = 0;
+while (i <=16) {
+    console.log(i);
+    i++;
+}
---
+++ javascript-para-iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,12 @@
+var nome = 'Raphael';
+
+var nomeMinusculo = nome.toLowerCase();
+
+var altura = 1.8;
+
+altura.toString();
+altura.toFixed();
+
+
+var btn = document.querySelector('.btn');
+
